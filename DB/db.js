42abb68a83b99fa5c3ffb2df3680db73afc521d3
Nilai Patel <nilaip96@gmail.com>
2021-03-25 14:16:04 -0700

    Changed host

--- DB/db.js
+++ DB/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'ubuntu',
-  host: 'localhost',
+  host: 'ec2-184-72-28-205.us-west-1.compute.amazonaws.com',
   database: 'product',
   password: 'ubuntu',
   port: 5432,
